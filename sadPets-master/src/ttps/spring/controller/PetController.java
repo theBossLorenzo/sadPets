1dd17287eb2e48f764315995eef9e6239826a590
theBossLorenzo <lorenzohandula@gmail.com>
2020-02-14 19:12:52 -0300

    Add files via upload

--- sadPets-master/src/ttps/spring/controller/PetController.java
+++ sadPets-master/src/ttps/spring/controller/PetController.java
@@ -5,8 +5,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import ttps.spring.clasesDAO.UserDAO;
+import ttps.spring.clasesDAO.VetDAO;
+import ttps.spring.interfacesDAO.VetDAOInterface;
+import ttps.spring.model.Owner;
 import ttps.spring.model.Pet;
+import ttps.spring.model.Vet;
+import ttps.spring.model.objectDTO;
 import ttps.spring.service.PetService;
+import ttps.spring.service.UserService;
 
 import java.util.List;
 
@@ -16,6 +24,10 @@ import java.util.List;
 public class PetController {
     @Autowired
     PetService petService;
+    @Autowired
+    UserService userService;
+    @Autowired
+    VetDAOInterface vetDao;
 
     @GetMapping("/pets/{id}")
     public ResponseEntity<List<Pet>> listAllPets(@PathVariable("id")long id){
@@ -27,12 +39,13 @@ public class PetController {
     }
     
     @PostMapping(value = "/pets")
-    public ResponseEntity<Pet> createPet(@RequestBody Pet pet){
-    	if(!petService.isPetExist(pet)) {
-    		return new ResponseEntity<Pet>(pet,HttpStatus.CONFLICT);
+    public ResponseEntity<Pet> createPet(@RequestBody objectDTO objeto){
+    	if(!petService.isPetExist(objeto.getPet())) {
+    		return new ResponseEntity<Pet>(objeto.getPet(),HttpStatus.CONFLICT);
     	}
-    	petService.savePet(pet);
-    	return new ResponseEntity<Pet>(pet,HttpStatus.OK);
+    	objeto.getPet().setOwner(objeto.getOwner());
+    	objeto.getPet().setVet(objeto.getVet());
+    	petService.savePet(objeto.getPet());
+    	return new ResponseEntity<Pet>(objeto.getPet(),HttpStatus.OK);
     }
-
 }
